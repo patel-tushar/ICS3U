d80b8226df6bd66808d9dfe36faf724c51ed8a1b
Tushar Patel <Tushar@10.17.194.28>
2017-02-21 11:22:19 -0500

    This is a push for hello world.

---
+++ ICS3U Random Stuff/src/Patel/HelloWorld.java
@@ -0,0 +1,16 @@
+package Patel;
+/**
+ * HelloWorld.java
+ * This program prints "Hello, World!" to the console
+ * 2017-02-21
+ * @author Tushar Patel
+ */
+
+public class HelloWorld {
+	public static void main(String[] args){
+		
+		//output hello
+		System.out.println("Hello, World!");
+	}
+	
+}
